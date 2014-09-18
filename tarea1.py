42c0dd3129b27527504d07878d5fa6d3689b1bf7
homeroid <ome_gonza@hotmail.com>
2014-09-18 12:55:57 -0600

    tarea variables

---
+++ tarea1.py
@@ -0,0 +1,34 @@
+import math 
+x = True
+print (x)
+x = not x
+print ( x )
+y = 4
+print (y)
+y = y +4
+print (y)
+y = y + 2.25
+print (y)
+y = round (y)
+print (y)
+y = y / 1.15
+print (y)
+z = 5 % 2
+print ( z)
+z = 4/2
+print (z)
+z = 4/3
+print (z)
+z = 9845**24
+print(z)
+a= "Hola variable"
+print (a)
+a = a + " tipo cadena"
+print (a)
+radio = 10 
+area = 3.1416 * (radio**2)
+print (area)
+gc= (12 * (9/5)) +32
+print (gc)
+imc = 72 / (1.85**2)
+print (imc)
